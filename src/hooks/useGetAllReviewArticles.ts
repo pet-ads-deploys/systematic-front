e835a1dde57dd7473b8ea0997121e500ecba172c
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-11-28 10:50:21 -0300

    refactor: changed the options code, to util getRequestOptions

--- src/hooks/useGetAllReviewArticles.ts
+++ src/hooks/useGetAllReviewArticles.ts
@@ -2,14 +2,12 @@ import { useEffect, useState } from 'react';
 import ArticleInterface from '../../public/interfaces/ArticleInterface';
 import axios from '../interceptor/interceptor';
 import { StudyInterface } from '../../public/interfaces/IStudy';
+import getRequestOptions from '../utils/getRequestOptions';
 
 const useGetAllReviewArticles = () => {
     const id = localStorage.getItem('systematicReviewId');
-    const token = localStorage.getItem('accessToken');
     const path = `http://localhost:8080/api/v1/systematic-study/${id}/study-review`;
-    const options = {
-        headers: {Authorization: `Bearer ${token}`}
-    }
+    const options = getRequestOptions();
     const [articles, setArticles] = useState<ArticleInterface[] | StudyInterface[]>([]);
 
     useEffect(() => {
