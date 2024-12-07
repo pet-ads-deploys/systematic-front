03207b8e4846e94e21f5ccc82ca1ced11a11b613
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-12-07 16:53:18 -0300

    feat: collapsable sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Box, Button } from '@chakra-ui/react';
 import { CloseIcon } from '@chakra-ui/icons';
@@ -11,13 +11,31 @@ interface Props {
 }
 
 const Sidebar = ({type}: Props) => {
+  const [isOpen, setIsOpen] = useState(true);
+ 
   return (
-    <motion.div className={Style.wrapper}>
-      <Box className={Style.closeBtn}>
-          <CloseButton />
-      </Box>
+    <motion.div 
+      className={ isOpen ? Style.wrapper : Style.collapsed}
+      animate={{
+        width: isOpen ? '250px' : '60px',
+        borderRadius: isOpen ? '30px' : '50%'
+      }}
+      transition={{ type: 'just', duration: 0.3 }}
+    >
+      {isOpen ?
+      ( 
+      <>
+        <Box className={Style.closeBtn}>
+            <CloseButton handleToggle={() => setIsOpen(!isOpen)} />
+        </Box>
 
-      <Navigation type={type}/>
+        <Navigation type={type}/> 
+      </>) :
+
+        (
+          <CloseButton className={Style.collapsedBtn} handleToggle={() => setIsOpen(!isOpen)} />
+        )
+      }
     </motion.div>
   )
 }
--- src/components/Sidebar/subComponents/CloseButton.tsx
+++ src/components/Sidebar/subComponents/CloseButton.tsx
@@ -2,9 +2,14 @@ import { CloseIcon } from '@chakra-ui/icons';
 import { Button } from '@chakra-ui/react';
 import React from 'react'
 
-const CloseButton = () => {
+interface Props {
+  handleToggle: () => void;
+  className?: string
+}
+
+const CloseButton = ({handleToggle, className = ''}: Props) => {
   return (
-    <Button w='0.2em' borderRadius='50%' bgColor='#263C56' _hover={{bgColor: "#263C56"}}>
+    <Button className={className} onClick={handleToggle} w='0.2em' borderRadius='50%' bgColor='#263C56' _hover={{bgColor: "#263C56"}}>
         <CloseIcon color='#c9d9e5' />
     </Button>
   )
