062ec9b7266145e223918a37c67e9c838984329c
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-12-07 18:34:28 -0300

    style: added tab on LogoutButton return

--- src/components/Sidebar/buttons/LogoutButton.tsx
+++ src/components/Sidebar/buttons/LogoutButton.tsx
@@ -7,15 +7,15 @@ import useLogout from '../../../hooks/logout/useLogout'
 const LogoutButton = () => {
     const logout = useLogout();
 
-  return (
-    <Box display='flex' w='120px' className={Style.linkBox}>
-        <Icon boxSize='21px' mr='7px' as={ImExit} color='#c9d9e5'/>
-        <Link 
-        className={Style.link} 
-        onClick={logout}
-        to='/'>Logout</Link>
-    </Box>
-  )
+    return (
+        <Box display='flex' w='120px' className={Style.linkBox}>
+            <Icon boxSize='21px' mr='7px' as={ImExit} color='#c9d9e5'/>
+            <Link 
+            className={Style.link} 
+            onClick={logout}
+            to='/'>Logout</Link>
+        </Box>
+    )
 }
 
 export default LogoutButton
