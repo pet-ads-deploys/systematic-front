89b23e4a27cf172d465e3d38bccaf6f214fbfe9b
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-12-10 21:27:06 -0300

    fix: centralized state using context

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,42 +1,51 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import { motion } from 'framer-motion';
 import { Box } from '@chakra-ui/react';
 import Style from './Sidebar.module.css';
 import CloseButton from './buttons/CloseButton';
 import Navigation from './Navigation';
+import AppContext from '../../components/Context/AppContext';
 
 interface Props {
-  type: string
+  type: string;
 }
 
-const Sidebar = ({type}: Props) => {
-  const [isOpen, setIsOpen] = useState(true);
- 
+const Sidebar = ({ type }: Props) => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('Failed to get the context in Sidebar.');
+  }
+
+  const { sidebarState, setSidebarState } = context;
+
+  const isOpen = sidebarState === 'open';
+
+  const toggleSidebar = () => {
+    setSidebarState(isOpen ? 'collapsed' : 'open');
+  };
+
   return (
-    <motion.div 
-      className={ isOpen ? Style.wrapper : Style.collapsed}
+    <motion.div
+      className={isOpen ? Style.wrapper : Style.collapsed}
       animate={{
         width: isOpen ? '250px' : '0px',
-        borderRadius: isOpen ? '30px' : '50%'
+        borderRadius: isOpen ? '30px' : '50%',
       }}
       transition={{ type: 'just', duration: 0.3 }}
     >
-      {isOpen ?
-      ( 
-      <>
-        <Box className={Style.closeBtn}>
-            <CloseButton isOpen={isOpen} handleToggle={() => setIsOpen(!isOpen)} />
-        </Box>
-
-        <Navigation type={type}/> 
-      </>) :
-
-        (
-          <CloseButton isOpen={isOpen} className={Style.collapsedBtn} handleToggle={() => setIsOpen(!isOpen)} />
-        )
-      }
+      {isOpen ? (
+        <>
+          <Box className={Style.closeBtn}>
+            <CloseButton isOpen={isOpen} handleToggle={toggleSidebar} />
+          </Box>
+          <Navigation type={type} />
+        </>
+      ) : (
+        <CloseButton isOpen={isOpen} className={Style.collapsedBtn} handleToggle={toggleSidebar} />
+      )}
     </motion.div>
-  )
-}
+  );
+};
 
 export default Sidebar;
