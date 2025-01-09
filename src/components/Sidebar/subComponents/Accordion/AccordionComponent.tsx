7eb338012b1df1afa45e745deacd8c2587c9b023
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-01-09 19:20:33 -0300

    feat: add icons to sections in sidebar

--- src/components/Sidebar/subComponents/Accordion/AccordionComponent.tsx
+++ src/components/Sidebar/subComponents/Accordion/AccordionComponent.tsx
@@ -1,7 +1,16 @@
 import { Accordion, AccordionItem, AccordionButton, Box, AccordionIcon, AccordionPanel } from "@chakra-ui/react"
-import NavItem from "../NavItem"
+// import NavItem from "../NavItem"
 import ProtocolAccordionSubItem from "./AccordionNavItem";
-import LogoutButton from "../../buttons/LogoutButton";
+// import LogoutButton from "../../buttons/LogoutButton";
+
+import { MdRule, MdOutlineArticle} from "react-icons/md";
+import { LuFileSearch, LuFileCheck2, LuTextSelect } from "react-icons/lu";
+import { CgFileAdd, CgCheckR } from "react-icons/cg";
+import { IoText, IoBarChartSharp} from "react-icons/io5";
+import { GrTag } from "react-icons/gr";
+import { TbFilterSearch } from "react-icons/tb";
+import { VscOpenPreview } from "react-icons/vsc";
+
 
 const AccordionComponent = () => {
     const id = localStorage.getItem('systematicReviewId');
@@ -11,47 +20,51 @@ const AccordionComponent = () => {
     <AccordionItem>
       <h2>
         <AccordionButton p='1.2vw 1vh' color="white">
-          <Box color='#c9d9e5' as='span' flex='1' textAlign='left'>
+          <Box color='#c9d9e5' as='span' flex='1' textAlign='left' display="flex" gap=".5rem">
+            <MdRule size="1.25rem" color="#c9d9e5"/>
             Protocol
           </Box>
           <AccordionIcon />
         </AccordionButton>
       </h2>
       <AccordionPanel pb={4}>
-        <ProtocolAccordionSubItem to={`/newReview/protocol/${id}`} text='Protocol' />
+        <ProtocolAccordionSubItem icon={<MdOutlineArticle size="1.2rem" color='#c9d9e5'/>} to={`/newReview/protocol/${id}`} text='Protocol' />
       </AccordionPanel>
     </AccordionItem>
   
     <AccordionItem>
       <h2>
         <AccordionButton p='1.2vw 1vh' color="white">
-          <Box color='#c9d9e5' as='span' flex='1' textAlign='left'>
+          <Box color='#c9d9e5' as='span' flex='1' textAlign='left' display="flex" gap=".5rem">
+            <LuFileSearch  size="1.1rem" color="#c9d9e5"/>
             Execution
           </Box>
           <AccordionIcon />
         </AccordionButton>
       </h2>
       <AccordionPanel pb={4}>
-        <ProtocolAccordionSubItem to={`/newReview/keywords`} text='Keywords' />
-        <ProtocolAccordionSubItem to={`/newReview/insertion`} text='Insertion' />
-        <ProtocolAccordionSubItem to={`/newReview/identification`} text='Identification' />
-        <ProtocolAccordionSubItem to={`/newReview/selection`} text='Selection' />
+        <ProtocolAccordionSubItem icon={<IoText size="1.1rem" color='#c9d9e5'/>} to={`/newReview/keywords`} text='Keywords' />
+        <ProtocolAccordionSubItem icon={<CgFileAdd  size="1.1rem" color='#c9d9e5'/>} to={`/newReview/insertion`} text='Insertion' />
+        <ProtocolAccordionSubItem icon={<GrTag size="1rem" color='#c9d9e5'/>} to={`/newReview/identification`} text='Identification' />
+        <ProtocolAccordionSubItem icon={<LuTextSelect size="1.2rem" color='#c9d9e5'/>} to={`/newReview/selection`} text='Selection' />
+        <ProtocolAccordionSubItem icon={<TbFilterSearch size="1.2rem" color='#c9d9e5'/>} to={`/newReview/extraction`} text='Extraction' />
       </AccordionPanel>
     </AccordionItem>
 
     <AccordionItem>
       <h2>
         <AccordionButton p='1.2vw 1vh' color="white">
-          <Box color='#c9d9e5' as='span' flex='1' textAlign='left'>
+          <Box color='#c9d9e5' as='span' flex='1' textAlign='left' display="flex" gap=".5rem">
+            <LuFileCheck2  size="1rem" color="#c9d9e5"/>
             Summarization
           </Box>
           <AccordionIcon />
         </AccordionButton>
       </h2>
       <AccordionPanel pb={4}>
-        <ProtocolAccordionSubItem to={`/newReview/graphics`} text='Graphics' />
-        <ProtocolAccordionSubItem to={`/newReview/visualization`} text='Visualization' />
-        <ProtocolAccordionSubItem to={`/newReview/finalization`} text='Finalization' />
+        <ProtocolAccordionSubItem icon={<IoBarChartSharp size="1rem" color='#c9d9e5'/>} to={`/newReview/graphics`} text='Graphics' />
+        <ProtocolAccordionSubItem icon={<VscOpenPreview size="1.2rem" color='#c9d9e5'/>} to={`/newReview/visualization`} text='Visualization' />
+        <ProtocolAccordionSubItem icon={<CgCheckR size="1rem" color='#c9d9e5'/>} to={`/newReview/finalization`} text='Finalization' />
       </AccordionPanel>
     </AccordionItem>
   </Accordion>
--- src/components/Sidebar/subComponents/Accordion/AccordionNavItem.tsx
+++ src/components/Sidebar/subComponents/Accordion/AccordionNavItem.tsx
@@ -1,19 +1,20 @@
-import { Box, Icon } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { Link } from 'react-router-dom'
 import Styles from './AccordionNavItem.module.css';
-import { ImExit } from "react-icons/im";
-import { FiPlusCircle } from "react-icons/fi";
-import { ImBooks } from "react-icons/im";
+// import { ImExit } from "react-icons/im";
+// import { FiPlusCircle } from "react-icons/fi";
+// import { ImBooks } from "react-icons/im";
 
 interface Props {
     to: string;
-    text: string
+    text: string;
+    icon: React.ReactNode;
 }
 
-const ProtocolAccordionSubItem = ({to, text}: Props) => {
+const ProtocolAccordionSubItem = ({to, text, icon}: Props) => {
   return (
-    <Box w='120px' display='flex' className={Styles.accordionNavlinkBox}>
-      <Icon color='#c9d9e5' />
+    <Box w='10rem' display='flex' className={Styles.accordionNavlinkBox} gap=".5rem">
+      {icon}
       <Link className={Styles.accordionLink} to={to}>{text}</Link>
     </Box>
   )
