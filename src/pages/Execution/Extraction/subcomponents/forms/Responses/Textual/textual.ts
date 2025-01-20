5804cae8ccf8d93d762c9ac11cf513aa85449cf3
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-01-20 15:24:00 -0300

    feat: create Textual response component

---
+++ src/pages/Execution/Extraction/subcomponents/forms/Responses/Textual/textual.ts
@@ -0,0 +1,13 @@
+export const container = {
+    p: ".5rem",
+    borderRadius: "1rem",
+    w: "100%"
+}
+
+export const label = {
+    fontSize: "1.5rem"
+}
+
+export const responseArea = {
+    bgColor:"white",
+}
