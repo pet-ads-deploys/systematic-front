5ac379bd0e5c1f704b7db3f4fa8a121d11189e1c
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-01-30 16:16:58 -0300

    feat: create style to buttons of nagivate in articles table

--- src/pages/Execution/Selection/subcomponents/ButtonsForSelection.tsx
+++ src/pages/Execution/Selection/subcomponents/ButtonsForSelection.tsx
@@ -1,67 +1,114 @@
-import { Button, Flex, IconButton, useDisclosure } from "@chakra-ui/react";
-import { boxconteiner, buttonconteiner, conteiner } from "../../styles/BtnSelectionStyles";
+// import { Button, Flex, IconButton, useDisclosure } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
+import {
+  boxconteiner,
+  buttonconteiner,
+  conteiner,
+  button,
+} from "../../styles/BtnSelectionStyles";
 import ComboBox from "../../../../components/Inputs/ComboBox";
-import { FaPen } from "react-icons/fa6";
+// import { FaPen } from "react-icons/fa6";
 import { useContext } from "react";
 import AppContext from "../../../../components/Context/AppContext";
 import StudySelectionContext from "../../../../components/Context/StudiesSelectionContext";
 import { StudyInterface } from "../../../../../public/interfaces/IStudy";
-import StudyEdtionModal from "../../../../components/Modals/StudyModal/StudyEdtionModal";
+// import StudyEdtionModal from "../../../../components/Modals/StudyModal/StudyEdtionModal";
 import useFetchInclusionCriteria from "../../../../hooks/fetch/useFetchInclusionCriteria";
 import useFetchExclusionCriteria from "../../../../hooks/fetch/useFetchExclusionCriterias";
+
+import { RiRestartFill } from "react-icons/ri";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export default function ButtonsForSelection() {
   const context = useContext(AppContext);
   const selectionContext = useContext(StudySelectionContext);
-  
+
   const isIncluded = selectionContext?.isIncluded;
   const isExcluded = selectionContext?.isExcluded;
-  const sortedStudies = (context?.selectionStudies as StudyInterface[]);
-  const index = (context?.selectionStudyIndex as number);
+  const sortedStudies = context?.selectionStudies as StudyInterface[];
+  const index = context?.selectionStudyIndex as number;
   const criteriosExclusao: string[] = useFetchExclusionCriteria();
   const criteriosInclusao: string[] = useFetchInclusionCriteria();
 
   function ChangeToNext() {
-    if (index < sortedStudies.length -1) {
+    if (index < sortedStudies.length - 1) {
       const newIndex = (index as number) + 1;
-      context?.setSelectionStudyIndex(newIndex)
-      context?.setSelectionStudy((sortedStudies as StudyInterface[])[newIndex])
+      context?.setSelectionStudyIndex(newIndex);
+      context?.setSelectionStudy((sortedStudies as StudyInterface[])[newIndex]);
     }
   }
 
   function ChangeToPrevius() {
     if (index >= 1) {
       const newIndex = (index as number) - 1;
-      context?.setSelectionStudyIndex(newIndex)
-      context?.setSelectionStudy((sortedStudies as StudyInterface[])[newIndex])
+      context?.setSelectionStudyIndex(newIndex);
+      context?.setSelectionStudy((sortedStudies as StudyInterface[])[newIndex]);
     }
   }
 
-  if(isExcluded != undefined && isIncluded != undefined)
-  return (
-    <>
-      <Flex sx={conteiner}>
-        <Flex sx={boxconteiner}>
-          
-          <ComboBox
-            isDisabled={isIncluded}
-            text="Exclude"
-            options={criteriosExclusao}
-          />
-          <ComboBox
-            isDisabled={isExcluded}
-            text="Include"
-            options={criteriosInclusao}
-          />
-        </Flex>
+  function ResetState() {
+    console.log("Preciso ver se a rota permite unclassified");
+  }
 
-        <Flex sx={buttonconteiner}>
-          <Button borderRadius={"3px"} onClick={ChangeToPrevius}>Previous</Button>
-          <Button borderRadius={"3px"} onClick={ChangeToNext}>Next</Button>
-        </Flex>
+  if (isExcluded != undefined && isIncluded != undefined)
+    return (
+      <>
+        <Flex sx={conteiner}>
+          <Flex sx={buttonconteiner}>
+            <Button
+              _hover={{
+                bg: "white",
+                color: "black",
+                border: "2px solid black",
+              }}
+              onClick={ChangeToPrevius}
+              sx={button}
+            >
+              <IoIosArrowBack size="1.5rem" /> Previous
+            </Button>
+          </Flex>
 
-        <Button borderRadius={"3px"}>Reset</Button>
-      </Flex>
-    </>
-  );
+          <Flex sx={boxconteiner}>
+            <ComboBox
+              isDisabled={isExcluded}
+              text="Include"
+              options={criteriosInclusao}
+            />
+            <ComboBox
+              isDisabled={isIncluded}
+              text="Exclude"
+              options={criteriosExclusao}
+            />
+            <Button
+              borderRadius="6px"
+              bg="#eab308"
+              color="white"
+              border="2px solid #f6bb42"
+              _hover={{bg:"white", color:"#eab308"}}
+              transition="0.2s ease-in-out"
+              boxShadow="md"
+              p="1rem"
+              onClick={ResetState}
+            >
+              Reset
+            </Button>
+          </Flex>
+
+          <Flex sx={buttonconteiner}>
+            <Button
+              _hover={{
+                bg: "white",
+                color: "black",
+                border: "2px solid black",
+              }}
+              onClick={ChangeToNext}
+              sx={button}
+            >
+              Next
+              <IoIosArrowForward size="1.5rem" />
+            </Button>
+          </Flex>
+        </Flex>
+      </>
+    );
 }
