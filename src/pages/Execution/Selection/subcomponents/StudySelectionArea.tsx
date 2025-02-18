12806133351e03885465291ec1f9eb0b43e13adb
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-02-18 12:01:18 -0300

    fix: correct index to studyData

--- src/pages/Execution/Selection/subcomponents/StudySelectionArea.tsx
+++ src/pages/Execution/Selection/subcomponents/StudySelectionArea.tsx
@@ -5,14 +5,16 @@ import { StudyInterface } from "../../../../../public/interfaces/IStudy";
 import { useContext, useEffect } from "react";
 import AppContext from "../../../../components/Context/AppContext";
 import useGetAllReviewArticles from "../../../../hooks/useGetAllReviewArticles";
-import StudySelectionContext, { StudySelectionProvider } from "../../../../components/Context/StudiesSelectionContext";
+import StudySelectionContext, {
+  StudySelectionProvider,
+} from "../../../../components/Context/StudiesSelectionContext";
 import { PageLayout } from "./LayoutFactory";
 
-export default function StudySelectionArea({type}:PageLayout) {
+export default function StudySelectionArea({ type }: PageLayout) {
   const context = useContext(AppContext);
   const selectionContext = useContext(StudySelectionContext);
-  if(!selectionContext) throw new Error("Failed to get selection context on study Selection area");
-  const reload = selectionContext.reload;
+  if (!selectionContext)
+    throw new Error("Failed to get selection context on study Selection area");
   const studyData = useGetAllReviewArticles();
   const showSelectionModal = context?.showSelectionModal;
   const setSelectionStudies = context?.setSelectionStudies;
@@ -20,24 +22,48 @@ export default function StudySelectionArea({type}:PageLayout) {
 
   useEffect(() => {
     console.log(studyIndex);
-  }, [studyIndex])
+  }, [studyIndex]);
 
-  if(setSelectionStudies && studyData?.articles) setSelectionStudies(studyData?.articles as StudyInterface[]);
+  if (setSelectionStudies && studyData?.articles)
+    setSelectionStudies(studyData?.articles as StudyInterface[]);
 
-  if (!showSelectionModal || studyIndex === null) return (
-    <Flex direction="column" bg="gray.600" borderRadius='1rem' w="100%" mb='20px' p="5" alignItems="center">
-      <Text color="white">Click on a study on the table</Text>
-    </Flex>
-  );
-  
+  if (!showSelectionModal || studyIndex === null)
+    return (
+      <Flex
+        direction="column"
+        bg="gray.600"
+        borderRadius="1rem"
+        w="100%"
+        mb="20px"
+        p="5"
+        alignItems="center"
+      >
+        <Text color="white">Click on a study on the table</Text>
+      </Flex>
+    );
+
   return (
     <StudySelectionProvider>
-      <Flex direction="column" borderRadius='1rem' bg="white" mb='20px' w="100%" h={type === "Extraction" ? "80%" : "100%"} p="5" alignItems={"center"}>
+      <Flex
+        direction="column"
+        borderRadius="1rem"
+        bg="white"
+        mb="20px"
+        w="100%"
+        h={type === "Extraction" ? "80%" : "100%"}
+        p="5"
+        alignItems={"center"}
+      >
         <Flex justifyContent="center" w="100%">
-        <ButtonsForSelection page={{type}}/>
+          <ButtonsForSelection page={{ type }} />
         </Flex>
         <Box w={"100%"} h="100%">
-          <StudyDataFiel studyData={(studyData?.articles?.[studyIndex] as StudyInterface)} page={{type}} />
+          {studyData?.articles && typeof studyIndex === "number" ? (
+            <StudyDataFiel
+              studyData={studyData?.articles?.[studyIndex] as StudyInterface}
+              page={{ type }}
+            />
+          ) : null}
         </Box>
       </Flex>
     </StudySelectionProvider>
