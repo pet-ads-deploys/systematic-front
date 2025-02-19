6a1f24b7334327607e0cde215803fce148d0fbf6
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-02-19 12:14:34 -0300

    fix: validations of types before dispatch request in custom hook

--- src/hooks/useComboBoxSelection.ts
+++ src/hooks/useComboBoxSelection.ts
@@ -1,43 +1,70 @@
+// External libraries
 import { useContext } from "react";
+
+// Contexts
 import StudySelectionContext from "../components/Context/StudiesSelectionContext";
 import AppContext from "../components/Context/AppContext";
+
+// Hooks
 import { UseChangeStudySelectionStatus } from "./useChangeStudySelectionStatus";
 import { UseChangeStudyExtractionStatus } from "./useChangeStudyExtractionStatus";
-import { PageLayout } from "../pages/Execution/Selection/subcomponents/LayoutFactory";
 
-interface ComboBoxSelectionProps{
-  page: PageLayout
+// Types
+import { PageLayout } from "../pages/Execution/Selection/subcomponents/LayoutFactory";
+import ArticleInterface from "../../public/interfaces/ArticleInterface";
+import { StudyInterface } from "../../public/interfaces/IStudy";
+
+interface ComboBoxSelectionProps {
+  page: PageLayout;
 }
 
-const useComboBoxSelection = ({page}: ComboBoxSelectionProps) => {
+const useComboBoxSelection = ({ page }: ComboBoxSelectionProps) => {
   const selectionContext = useContext(StudySelectionContext);
   const appContext = useContext(AppContext);
   const setIsIncluded = selectionContext?.setIsIncluded;
   const setIsExcluded = selectionContext?.setIsExcluded;
+  const articles = selectionContext?.articles;
+  const articleIndex = appContext?.selectionStudyIndex;
+  let article: ArticleInterface | StudyInterface;
 
+  if (articles && articleIndex) article = articles[articleIndex];
+
+  // Functions
   const handleIncludeItemClick = (isChecked: boolean) => {
-    if(setIsIncluded) setIsIncluded(isChecked);
-    const articles = selectionContext?.articles;
-    const articleIndex = appContext?.selectionStudyIndex;
-    
-    if(articles && articleIndex){
-      const studyReviewId = articles[articleIndex].studyReviewId;
-      page.type === "Selection" ? UseChangeStudySelectionStatus({studyReviewId, status: 'INCLUDED'}) : UseChangeStudyExtractionStatus({studyReviewId, status: 'INCLUDED'})
-      selectionContext.reloadArticles();
+    if (setIsIncluded) setIsIncluded(isChecked);
+    if (articles && articleIndex) {
+      if (article && "studyReviewId" in article) {
+        page.type === "Selection"
+          ? UseChangeStudySelectionStatus({
+              studyReviewId: article.studyReviewId,
+              status: "INCLUDED",
+            })
+          : UseChangeStudyExtractionStatus({
+              studyReviewId: article.studyReviewId,
+              status: "INCLUDED",
+            });
+        selectionContext.reloadArticles();
+      }
     }
   };
 
   const handleExcludeItemClick = (isChecked: boolean) => {
-    if(setIsExcluded) setIsExcluded(isChecked);
-    const articles = selectionContext?.articles;
-    const articleIndex = appContext?.selectionStudyIndex;
-    
-    if(articles && articleIndex){
-      const studyReviewId = articles[articleIndex].studyReviewId;
-      page.type === "Selection" ? UseChangeStudySelectionStatus({studyReviewId, status: 'EXCLUDED'}) : UseChangeStudyExtractionStatus({studyReviewId, status: 'EXCLUDED'})
-      selectionContext.reloadArticles();
+    if (setIsExcluded) setIsExcluded(isChecked);
+    if (articles && articleIndex) {
+      if (article && "studyReviewId" in article) {
+        page.type === "Selection"
+          ? UseChangeStudySelectionStatus({
+              studyReviewId: article.studyReviewId,
+              status: "EXCLUDED",
+            })
+          : UseChangeStudyExtractionStatus({
+              studyReviewId: article.studyReviewId,
+              status: "EXCLUDED",
+            });
+        selectionContext.reloadArticles();
+      }
     }
-  }
+  };
 
   return { handleIncludeItemClick, handleExcludeItemClick };
 };
--- src/hooks/useResetStatus.ts
+++ src/hooks/useResetStatus.ts
@@ -1,8 +1,15 @@
+// External libraries
 import { useContext } from "react";
+
+// Contexts
 import StudySelectionContext from "../components/Context/StudiesSelectionContext";
 import AppContext from "../components/Context/AppContext";
+
+// Hooks
 import { UseChangeStudySelectionStatus } from "./useChangeStudySelectionStatus";
 import { UseChangeStudyExtractionStatus } from "./useChangeStudyExtractionStatus";
+
+//Types
 import { PageLayout } from "../pages/Execution/Selection/subcomponents/LayoutFactory";
 
 interface ResetButtonProps {
@@ -13,23 +20,26 @@ const useResetStatus = ({ page }: ResetButtonProps) => {
   const selectionContext = useContext(StudySelectionContext);
   const appContext = useContext(AppContext);
 
+  // Functions
   const handleResetStatusToUnclassified = () => {
     const articles = selectionContext?.articles;
     const articleIndex = appContext?.selectionStudyIndex;
 
     if (articles && articleIndex) {
-      const studyReviewId = articles[articleIndex].studyReviewId;
-      page.type === "Selection"
-        ? UseChangeStudySelectionStatus({
-            studyReviewId,
-            status: "UNCLASSIFIED",
-          })
-        : UseChangeStudyExtractionStatus({
-            studyReviewId,
-            status: "UNCLASSIFIED",
-          });
+      const article = articles[articleIndex];
+      if (article && "studyReviewId" in article) {
+        page.type === "Selection"
+          ? UseChangeStudySelectionStatus({
+              studyReviewId: article.studyReviewId,
+              status: "UNCLASSIFIED",
+            })
+          : UseChangeStudyExtractionStatus({
+              studyReviewId: article.studyReviewId,
+              status: "UNCLASSIFIED",
+            });
+      }
+      selectionContext?.reloadArticles();
     }
-    selectionContext?.reloadArticles();
   };
 
   return { handleResetStatusToUnclassified };
