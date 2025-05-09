efc8ef0ce1dfcc731c260aab6cc865a405da850c
guilhermeAm <guilherme.andmendes03@gmail.com>
2025-05-09 15:41:44 -0300

    feat: render questions forms in extraction screen

--- src/components/Modals/StudyModal/ArticleExtractionData.tsx
+++ src/components/Modals/StudyModal/ArticleExtractionData.tsx
@@ -1,8 +1,53 @@
-import { ArticlePreviewProps } from "./StudyData";
+import { useState } from "react";
+
 import ExtractionForm from "../../../pages/Execution/Extraction/subcomponents/forms/ExtractionForm/ExtractionForm";
+import RiskOfBiasForm from "../../../pages/Execution/Extraction/subcomponents/forms/RobForm/RobForm";
+
+import { ArticlePreviewProps } from "./StudyData";
+
+import { Box, Button, Flex } from "@chakra-ui/react";
+
+type FormType = "EXTRACTION" | "RISK_OF_BIAS";
+
+const buttonStyles = {
+  bg: "white",
+  borderRadius: ".5rem .5rem 0 0",
+};
+
+const forms = [
+  { label: "Extraction", type: "EXTRACTION" },
+  { label: "Risk Of Bias", type: "RISK_OF_BIAS" },
+] as const;
 
 export default function ArticlesExtrationData({
   studyData,
 }: ArticlePreviewProps) {
-  return <ExtractionForm studyData={studyData} />;
+  const [formVisible, setFormVisible] = useState<FormType>("EXTRACTION");
+
+  return (
+    <Box w="100%" h="calc(100vh - 10rem)" bg="white" gap="3rem">
+      <Flex w="100%" align="center" justifyContent="start">
+        {forms.map((form) => (
+          <Button
+            key={form.type}
+            sx={buttonStyles}
+            border={formVisible === form.type ? "2px solid black" : "none"}
+            borderBottom={
+              formVisible === form.type ? "none" : "2px solid black"
+            }
+            onClick={() => setFormVisible(form.type)}
+          >
+            {form.label}
+          </Button>
+        ))}
+      </Flex>
+      <Box mt="2rem">
+        {formVisible === "EXTRACTION" ? (
+          <ExtractionForm studyData={studyData} />
+        ) : (
+          <RiskOfBiasForm studyData={studyData} />
+        )}
+      </Box>
+    </Box>
+  );
 }
