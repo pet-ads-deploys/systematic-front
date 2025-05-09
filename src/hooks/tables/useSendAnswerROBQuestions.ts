8d5a7df4c3c3fd824caa17232fc1de364c81ff4c
guilhermeAm <guilherme.andmendes03@gmail.com>
2025-05-09 15:10:20 -0300

    feat: create hook to answer risk of bias questions

---
+++ src/hooks/tables/useSendAnswerROBQuestions.ts
@@ -0,0 +1,60 @@
+import { useContext } from "react";
+import Axios from "../../interceptor/interceptor";
+
+import StudySelectionContext from "../../components/Context/StudiesSelectionContext";
+import AppContext from "../../components/Context/AppContext";
+
+import { StudyInterface } from "../../../public/interfaces/IStudy";
+import ArticleInterface from "../../../public/interfaces/ArticleInterface";
+import { HandleSendAnswerProps } from "../../pages/Execution/Extraction/subcomponents/forms/types";
+
+import getRequestOptions from "../../utils/getRequestOptions";
+
+export function useSendAnswerROBQuestions() {
+  const selectionContext = useContext(StudySelectionContext);
+  const appContext = useContext(AppContext);
+  const articleIndex = appContext?.selectionStudyIndex;
+  const articles = selectionContext?.articles;
+  let article: ArticleInterface | StudyInterface;
+
+  if (articles && articleIndex) article = articles[articleIndex];
+
+  const sendAnswerROBQuestions = async ({
+    questionId,
+    type,
+    answer,
+  }: HandleSendAnswerProps) => {
+    if (articleIndex === undefined || !selectionContext) {
+      console.warn("Context not available, cannot change priority.");
+      return;
+    }
+    try {
+      const id = localStorage.getItem("systematicReviewId");
+      const options = getRequestOptions();
+
+      if (articles && articleIndex) {
+        if (article && "studyReviewId" in article) {
+          const path = `http://localhost:8080/api/v1/systematic-study/${id}/study-review/${[
+            article.studyReviewId,
+          ]}/riskOfBias-answer`;
+          await Axios.patch(
+            path,
+            {
+              questionId,
+              answer,
+              type,
+            },
+            options
+          );
+          selectionContext.reloadArticles();
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return {
+    sendAnswerROBQuestions,
+  };
+}
