c5a147ab1ebcc974fc2a948ff943bc9104385ddc
guilhermeAm <guilherme.andmendes03@gmail.com>
2025-05-09 19:28:16 -0300

    refactor: separate header of article in forms

--- src/components/Modals/StudyModal/ArticleExtractionData.tsx
+++ src/components/Modals/StudyModal/ArticleExtractionData.tsx
@@ -6,6 +6,9 @@ import RiskOfBiasForm from "../../../pages/Execution/Extraction/subcomponents/fo
 import { ArticlePreviewProps } from "./StudyData";
 
 import { Box, Button, Flex } from "@chakra-ui/react";
+import HeaderForm from "../../../pages/Execution/Extraction/subcomponents/forms/HeaderForm/HeaderForm";
+import { CheckCircleIcon, InfoIcon, WarningIcon } from "@chakra-ui/icons";
+import { IoIosCloseCircle } from "react-icons/io";
 
 type FormType = "EXTRACTION" | "RISK_OF_BIAS";
 
@@ -19,13 +22,59 @@ const forms = [
   { label: "Risk Of Bias", type: "RISK_OF_BIAS" },
 ] as const;
 
+const statusIconMap: Record<
+  string,
+  {
+    icon: React.ReactNode;
+    color: string;
+  }
+> = {
+  INCLUDED: {
+    icon: <CheckCircleIcon color="green.500" />,
+    color: "green",
+  },
+  DUPLICATED: {
+    icon: <InfoIcon color="blue.500" />,
+    color: "blue",
+  },
+  EXCLUDED: {
+    icon: <IoIosCloseCircle color="red.500" size="1.4rem" />,
+    color: "red",
+  },
+  UNCLASSIFIED: {
+    icon: <WarningIcon color="yellow.500" />,
+    color: "yellow",
+  },
+};
+
 export default function ArticlesExtrationData({
   studyData,
 }: ArticlePreviewProps) {
   const [formVisible, setFormVisible] = useState<FormType>("EXTRACTION");
+  const extractionStatus = statusIconMap[studyData.extractionStatus];
 
   return (
     <Box w="100%" h="calc(100vh - 10rem)" bg="white" gap="3rem">
+      <HeaderForm text={studyData.title} />
+      <Flex gap="2rem">
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          fontFamily="sans-serif"
+          fontSize="1rem"
+          p=".5rem .25rem"
+          gap="1rem"
+          borderRadius="1rem"
+          bg={`${extractionStatus.color}.200`}
+          color={`${extractionStatus.color}.800`}
+          maxW="10rem"
+          w="10rem"
+          h="2rem"
+        >
+          {extractionStatus.icon}
+          {studyData.extractionStatus}
+        </Flex>
+      </Flex>
       <Flex w="100%" align="center" justifyContent="start">
         {forms.map((form) => (
           <Button
@@ -42,11 +91,7 @@ export default function ArticlesExtrationData({
         ))}
       </Flex>
       <Box mt="2rem">
-        {formVisible === "EXTRACTION" ? (
-          <ExtractionForm studyData={studyData} />
-        ) : (
-          <RiskOfBiasForm studyData={studyData} />
-        )}
+        {formVisible === "EXTRACTION" ? <ExtractionForm /> : <RiskOfBiasForm />}
       </Box>
     </Box>
   );
--- src/pages/Execution/Extraction/subcomponents/forms/RobForm/RobForm.tsx
+++ src/pages/Execution/Extraction/subcomponents/forms/RobForm/RobForm.tsx
@@ -2,24 +2,19 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Button, Flex, FormControl, Text } from "@chakra-ui/react";
 
-import HeaderForm from "../HeaderForm/HeaderForm";
-
 import { useFetchRobQuestions } from "../../../../../../hooks/fetch/useFetchRobQuestions.tsx";
 import { useSendAnswerROBQuestions } from "../../../../../../hooks/tables/useSendAnswerROBQuestions.ts";
+import { useSubmitAnswerForm } from "../../../../../../hooks/reviews/forms/useSubmitAnswerForm.tsx";
 
 import { AnswerProps } from "../types.ts";
-import { ArticlePreviewProps } from "../../../../../../components/Modals/StudyModal/StudyData.tsx";
 
 import { createResponseComponent } from "../utils/createResponseComponents.tsx";
 
 import { button } from "../styles.ts";
 
-import { CheckCircleIcon, InfoIcon, WarningIcon } from "@chakra-ui/icons";
 import { FaPlusCircle } from "react-icons/fa";
-import { IoIosCloseCircle } from "react-icons/io";
-import { useSubmitAnswerForm } from "../../../../../../hooks/reviews/forms/useSubmitAnswerForm.tsx";
 
-export default function RiskOfBiasForm({ studyData }: ArticlePreviewProps) {
+export default function RiskOfBiasForm() {
   const [responses, setResponses] = useState<Record<string, AnswerProps>>({});
   const updateResponse = (questionId: string, response: AnswerProps) => {
     setResponses((prev) => ({
@@ -40,57 +35,10 @@ export default function RiskOfBiasForm({ studyData }: ArticlePreviewProps) {
     handleSendAnswer: sendAnswerROBQuestions,
   });
 
-  const statusIconMap: Record<
-    string,
-    {
-      icon: React.ReactNode;
-      color: string;
-    }
-  > = {
-    INCLUDED: {
-      icon: <CheckCircleIcon color="green.500" />,
-      color: "green",
-    },
-    DUPLICATED: {
-      icon: <InfoIcon color="blue.500" />,
-      color: "blue",
-    },
-    EXCLUDED: {
-      icon: <IoIosCloseCircle color="red.500" size="1.4rem" />,
-      color: "red",
-    },
-    UNCLASSIFIED: {
-      icon: <WarningIcon color="yellow.500" />,
-      color: "yellow",
-    },
-  };
-
-  const extractionStatus = statusIconMap[studyData.extractionStatus];
-
   const hasQuestions = questions ? questions.length > 0 : false;
 
   return (
     <FormControl w="100%" height="100%" gap="3rem" bg="white" overflowY="auto">
-      <Flex gap="2rem">
-        <Flex
-          justifyContent="center"
-          alignItems="center"
-          fontFamily="sans-serif"
-          fontSize="1rem"
-          p=".5rem .25rem"
-          gap="1rem"
-          borderRadius="1rem"
-          bg={`${extractionStatus.color}.200`}
-          color={`${extractionStatus.color}.800`}
-          maxW="10rem"
-          w="10rem"
-          h="2rem"
-        >
-          {extractionStatus.icon}
-          {studyData.extractionStatus}
-        </Flex>
-      </Flex>
-      <HeaderForm text={studyData.title} />
       <Box gap="5rem">
         {hasQuestions ? (
           questions?.map((question) =>
