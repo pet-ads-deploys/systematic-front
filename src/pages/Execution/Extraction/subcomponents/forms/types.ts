557d9818170d065d151afbe5ccef014339a8157f
guilhermeAm <guilherme.andmendes03@gmail.com>
2025-05-09 13:51:29 -0300

    refactor: create files to separate types logic and styles in data extraction forms

---
+++ src/pages/Execution/Extraction/subcomponents/forms/types.ts
@@ -0,0 +1,29 @@
+export interface Questions {
+  code: string;
+  description: string;
+  lower: number;
+  higher: number;
+  options: string[] | null;
+  questionId: string | null;
+  questionType: string | null;
+  scales: Record<string, number>;
+  systematicStudyId: string | null;
+  context: string;
+}
+
+export type TypeOfQuestions =
+  | "TEXTUAL"
+  | "NUMBERED_SCALE"
+  | "LABELED_SCALE"
+  | "PICK_LIST";
+
+export type AnswerProps = {
+  value: string | number | { name: string; value: number };
+  type: TypeOfQuestions;
+};
+
+export interface HandleSendAnswerProps {
+  questionId: string;
+  answer: string | number | { name: string; value: number };
+  type: TypeOfQuestions;
+}
