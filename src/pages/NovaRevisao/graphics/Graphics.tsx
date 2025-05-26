fb799c457020bba9a427bad9d68c9aefc5bb0108
montillha <marianaalmeida235b@gmail.com>
2025-05-26 12:40:19 -0300

    style: update container and chart box styles

--- src/pages/NovaRevisao/graphics/Graphics.tsx
+++ src/pages/NovaRevisao/graphics/Graphics.tsx
@@ -1,27 +1,62 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import Header from "../../../components/ui/Header/Header";
 
 import PieChart from "../../../components/Charts/PieChart/PieChart";
-import { barchartBox, conteiner, graphicsconteiner, piechartBox } from "../styles/graphicsStyles";
+import { barchartBox, conteiner, graphicsconteiner, piechartBox, textDescription } from "../styles/graphicsStyles";
 import FlexLayout from "../../../components/ui/Flex/Flex";
 import BarChart from "../../../components/Charts/BarChart/BarChart";
+import FunnelChart from "../../../components/Charts/FunnelChart/FunnelChart";
 
 export default function Graphics() {
   return (
     <FlexLayout navigationType="Accordion" defaultOpen={2}>
       <Header text="Graphics" />
-      <Box sx={conteiner} bg={'red'}>
+      <Box sx={conteiner} >
+
+        <Text sx={textDescription}>Search Sorces</Text>
         <Box sx={graphicsconteiner}>
           <Box sx={piechartBox}>
-            <PieChart />
+            <PieChart/>
           </Box>
         </Box>
+
+         <Text sx={textDescription}>First Selection</Text>
         <Box sx={graphicsconteiner}>
           <Box sx={barchartBox}>
-            <BarChart />
+            <BarChart criteria="inclusion" />
+          </Box>
+          <Box sx={barchartBox} >
+            <BarChart  criteria="exclusion" />
           </Box>
         </Box>
+
+        <Text sx={textDescription}>Second Selection</Text>
+        <Box sx={graphicsconteiner}>
+          <Box sx={barchartBox}>
+            <BarChart criteria="inclusion" />
+          </Box>
+          <Box sx={barchartBox}>
+            <BarChart criteria="exclusion" />
+          </Box>
+        </Box>
+
+
+        <Text sx={textDescription}>Studies Funnel</Text>
+        <Box sx={graphicsconteiner}>
+          <Box>
+            <FunnelChart />
+          </Box>
+        </Box>
+
+          <Text sx={textDescription}>Included Studies</Text>
+
       </Box>
+
+    
+
+
+
+
     </FlexLayout>
   );
 }
