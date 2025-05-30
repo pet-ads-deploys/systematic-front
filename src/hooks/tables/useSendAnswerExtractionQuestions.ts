c192eebd3ead63f4a291c7f584c8c83d1039a4bb
guilhermeAm <guilherme.andmendes03@gmail.com>
2025-05-30 01:27:10 -0300

    refactor: use custom hook to get current id

--- src/hooks/tables/useSendAnswerExtractionQuestions.ts
+++ src/hooks/tables/useSendAnswerExtractionQuestions.ts
@@ -1,54 +1,48 @@
+// External library
 import { useContext } from "react";
+
+// Service
 import Axios from "../../interceptor/interceptor";
 
+// Context
 import StudySelectionContext from "../../components/Context/StudiesSelectionContext";
-import AppContext from "../../components/Context/AppContext";
 
-import { StudyInterface } from "../../../public/interfaces/IStudy";
-import ArticleInterface from "../../../public/interfaces/ArticleInterface";
-import { HandleSendAnswerProps } from "../../pages/Execution/Extraction/subcomponents/forms/types";
+// Hooks
+import useFocusedArticle from "../reviews/useFocusedArticle";
 
+// Utils
 import getRequestOptions from "../../utils/getRequestOptions";
+
+// Types
+import type { HandleSendAnswerProps } from "../../pages/Execution/Extraction/subcomponents/forms/types";
 
 export function useSendAnswerExtractionQuestions() {
   const selectionContext = useContext(StudySelectionContext);
-  const appContext = useContext(AppContext);
-  const articleIndex = appContext?.selectionStudyIndex;
-  const articles = selectionContext?.articles;
-  let article: ArticleInterface | StudyInterface;
-
-  if (articles && articleIndex) article = articles[articleIndex];
+  const { articleInFocus } = useFocusedArticle({ page: "Extraction" });
 
   const sendAnswerExtractionQuestions = async ({
     questionId,
     type,
     answer,
   }: HandleSendAnswerProps) => {
-    if (articleIndex === undefined || !selectionContext) {
-      console.warn("Context not available, cannot change priority.");
+    if (!articleInFocus || !selectionContext) {
+      console.warn("Context not available, cannot send answer from the question.");
       return;
     }
     try {
       const id = localStorage.getItem("systematicReviewId");
       const options = getRequestOptions();
-
-      if (articles && articleIndex) {
-        if (article && "studyReviewId" in article) {
-          const path = `http://localhost:8080/api/v1/systematic-study/${id}/study-review/${[
-            article.studyReviewId,
-          ]}/extraction-answer`;
-          await Axios.patch(
-            path,
-            {
-              questionId,
-              answer,
-              type,
-            },
-            options
-          );
-          selectionContext.reloadArticles();
-        }
-      }
+      const path = `http://localhost:8080/api/v1/systematic-study/${id}/study-review/${articleInFocus.studyReviewId}/extraction-answer`;
+      await Axios.patch(
+        path,
+        {
+          questionId,
+          answer,
+          type,
+        },
+        options
+      );
+      selectionContext.reloadArticles();
     } catch (error) {
       console.log(error);
     }
