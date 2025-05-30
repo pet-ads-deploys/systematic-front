2df825ad0610e4b4dc424c6fc6673649e9772f60
Matheus <matheusg.farias@hotmail.com>
2025-05-30 13:58:35 -0300

    fix: comment out unused parameter

--- src/hooks/useNumberScale.tsx
+++ src/hooks/useNumberScale.tsx
@@ -1,20 +1,21 @@
 import { useState } from 'react'
 
 const useNumberScale = () => {
-  const [ minimalValue, setMinimalValue ] = useState(0);
-  const [ maximalValue, setMaximalValue] = useState(10);
+  const [minimalValue, setMinimalValue] = useState(0);
+  const [maximalValue, setMaximalValue] = useState(10);
 
-  function handleMinimalValue(valueAsString: string, valueAsNumber: number){
+  // valueAsString: string
+  function handleMinimalValue(valueAsNumber: number) {
     if (!isNaN(valueAsNumber)) {
-        setMinimalValue(valueAsNumber);
+      setMinimalValue(valueAsNumber);
     }
-}
-
-function handleMaximalValue(valueAsString: string, valueAsNumber: number){
+  }
+  // valueAsString: string
+  function handleMaximalValue(valueAsNumber: number) {
     if (!isNaN(valueAsNumber)) {
-        setMaximalValue(valueAsNumber);
+      setMaximalValue(valueAsNumber);
     }
-}
+  }
 
   return { handleMaximalValue, handleMinimalValue, minimalValue, maximalValue };
 }
