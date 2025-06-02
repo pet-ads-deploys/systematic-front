ef096323d1cd9b83a3f6edaacf133c2020113061
montillha <marianaalmeida235b@gmail.com>
2025-06-02 11:56:08 -0300

    feat: add initial structure for LineChart component

---
+++ src/components/Charts/LineChart.tsx/LineChart.tsx
@@ -0,0 +1,66 @@
+import { ApexOptions } from "apexcharts";
+import Chart from "react-apexcharts";
+import { useState } from "react";
+
+export default function LineChart() {
+  const [chartConfig, setChartConfig] = useState<{
+    series: { name: string; data: number[] }[];
+    options: ApexOptions;
+  }>({
+    series: [
+      {
+        name: "studies",
+        data: [3, 4, 6, 8, 13, 7, 24, 3, 6, 9, 12, 23],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        toolbar: {
+          show: true,
+          tools: {
+            selection: true,
+            download: true,
+            zoom: false,
+            zoomin: false,
+            zoomout: false,
+            pan: false,
+            reset: false,
+          },
+        },
+      },
+      colors: ["#3c73b6"],
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "straight",
+      },
+      title: {
+        text: "Included Studies by Year",
+        align: "left",
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        categories: [
+          2013, 2014, 2015, 2016, 2018, 2019, 2020, 2021, 2022, 2023, 2024,
+          2025,
+        ],
+      },
+    },
+  });
+  return (
+    <Chart
+      options={chartConfig.options}
+      series={chartConfig.series}
+      type="line"
+      height={450}
+      width={700}
+    />
+  );
+}
