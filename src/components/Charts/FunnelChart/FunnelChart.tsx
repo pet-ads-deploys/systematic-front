705dc5cf5538fc74cc77b637cbc4cf9a464c7049
guilhermeAm <guilherme.andmendes03@gmail.com>
2025-06-20 17:30:56 -0300

    refactor: remove unnecessary state in funnel component

--- src/components/Charts/FunnelChart/FunnelChart.tsx
+++ src/components/Charts/FunnelChart/FunnelChart.tsx
@@ -1,65 +1,66 @@
-
-import { useState } from "react";
+// External library
 import { ApexOptions } from "apexcharts";
 import Chart from "react-apexcharts";
 
+// Types
+interface FunnelProps {
+  series: {
+    name: string;
+    data: number[];
+  }[];
+  options: ApexOptions;
+}
 
-
-export default function FunnelChart(){
-    const [funnelConfing,setFunnelConfig] = useState<{
-        series:{name:string,data:number[]}[],
-        options:ApexOptions;
-    }>({
-        series:[
-            {
-                name:"total studies:",
-                data: [1200, 990, 548, 200, 50],
-            }
-
+export default function FunnelChart() {
+  const funnelConfig: FunnelProps = {
+    series: [
+      {
+        name: "Total studies:",
+        data: [1200, 990, 548, 200, 50],
+      },
+    ],
+    options: {
+      chart: {
+        type: "bar",
+        dropShadow: {
+          enabled: true,
+        },
+      },
+      colors: ["#3c73b6"],
+      plotOptions: {
+        bar: {
+          borderRadius: 0,
+          horizontal: true,
+          barHeight: "80%",
+          isFunnel: true,
+        },
+      },
+      dataLabels: {
+        enabled: true,
+      },
+      title: {
+        text: "Studies Funnel",
+        align: "left",
+      },
+      xaxis: {
+        categories: [
+          "Identificados",
+          "Sem duplicados",
+          "Após título/resumo",
+          "Lidos",
+          "Incluídos na revisão",
         ],
-        options:{
-            chart:{
-                type:"bar",
-                dropShadow:{
-                    enabled:true
-                },
+      },
+    },
+  };
 
-            },
-              colors: ['#3c73b6'], 
-             plotOptions:{
-                bar:{
-                    borderRadius:0,
-                    horizontal:true,
-                    barHeight:"80%",
-                    isFunnel:true,
-                }
-
-            },
-            dataLabels:{
-                enabled:true,
-            },
-            title:{
-                text:'Studies Funnel',
-                align:"left"
-            },
-            xaxis:{
-                categories:[
-                    "Identificados",
-                    "Sem duplicados",
-                    "Após título/resumo",
-                    "Lidos",
-                    "Incluídos na revisão"
-                ]
-
-            },
-        }
-       
-    });
-    return (
-        <Chart options={funnelConfing.options}
-        series={funnelConfing.series}
-        type="bar"
-        height={500}
-        width={800}/>
-    );
+  return (
+    <Chart
+      options={funnelConfig.options}
+      series={funnelConfig.series}
+      type={funnelConfig.options.chart?.type}
+      height={500}
+      width={800}
+    />
+  );
 }
