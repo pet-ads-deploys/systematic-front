f644d7d26cb05d37fbf6b7032a82281f8455dac9
Guilherme A Mendes <guilherme.andmendes03@gmail.com>
2025-07-30 13:20:52 -0300

    feat: create the authcontext with responsability for centralizing and encapsulating the logic for restoring user data, login and logout

---
+++ src/features/auth/context/AuthContext.tsx
@@ -0,0 +1,140 @@
+// External library
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Hooks
+import useDecodeToken from "@features/auth/hooks/useDecodeToken";
+
+// Services
+import loginService from "@features/auth/services/useLoginUser";
+import logoutService from "@features/auth/services/useLogout";
+
+// Utils
+import { userStorage } from "@features/auth/utils/userStorage";
+
+// Factory
+import errorFactory from "@features/shared/errors/factory/errorFactory";
+
+// Error
+import { Either } from "@features/shared/errors/pattern/Either";
+import { ApplicationError } from "@features/shared/errors/base/ApplicationError";
+
+// Guards
+import { isLeft, right } from "@features/shared/errors/pattern/Either";
+
+// Types
+import type { AccessCredentials, UserData } from "@features/auth/types";
+
+interface AuthContextProps {
+  user: UserData | null;
+  isLoading: boolean;
+  login: (
+    credentials: AccessCredentials
+  ) => Promise<Either<ApplicationError, void>>;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextProps | undefined>(
+  undefined
+);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<UserData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { decodeToken, checkTokenExpiration } = useDecodeToken();
+  const navigate = useNavigate();
+
+  const initialize = (): Either<ApplicationError, UserData> => {
+    setIsLoading(true);
+    const restoreUserData = userStorage.get();
+
+    if (isLeft(restoreUserData)) {
+      setIsLoading(false);
+      return restoreUserData;
+    }
+
+    const userData: UserData = restoreUserData.value;
+
+    if (!checkTokenExpiration(userData)) {
+      setIsLoading(false);
+      return errorFactory("unauthorized", "Token expired.");
+    }
+
+    return right(userData);
+  };
+
+  useEffect(() => {
+    const restoredUser = initialize();
+
+    if (isLeft(restoredUser)) {
+      logout();
+      setIsLoading(false);
+      return;
+    }
+
+    setUser(restoredUser.value);
+    setIsLoading(false);
+  }, []);
+
+  const login = async (
+    credentials: AccessCredentials
+  ): Promise<Either<ApplicationError, void>> => {
+    try {
+      const response = await loginService(credentials);
+
+      if (!response || !response.data) {
+        return errorFactory("custom", "Request is empty.");
+      }
+
+      const { token } = response.data;
+
+      if (!token) {
+        return errorFactory("unauthorized", "Token not found.");
+      }
+
+      const decoded = decodeToken(token);
+
+      if (!checkTokenExpiration(decoded)) {
+        return errorFactory("unauthorized", "Expires Token.");
+      }
+
+      const userData: UserData = {
+        ...decoded,
+        token,
+      };
+
+      userStorage.set(userData);
+      navigate("/user");
+
+      return right(undefined);
+    } catch (error) {
+      return errorFactory(
+        "unauthorized",
+        "Username or password incorrect. Try again with other credentials."
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const logout = () => {
+    userStorage.clear();
+    logoutService();
+    setUser(null);
+    navigate("/");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isLoading,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
