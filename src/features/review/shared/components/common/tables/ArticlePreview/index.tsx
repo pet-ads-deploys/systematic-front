4de9c259df085dd7baf23a5034306bb629c0659a
DylanPetrucelli <dtpetrucelli@gmail.com>
2025-08-11 14:38:11 -0300

    refactor: Create article header component

--- src/features/review/shared/components/common/tables/ArticlePreview/index.tsx
+++ src/features/review/shared/components/common/tables/ArticlePreview/index.tsx
@@ -1,79 +1,10 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 import { ArticlePreviewProps } from "../StudyData";
 
-import { CheckCircleIcon, InfoIcon, WarningIcon } from "@chakra-ui/icons";
-import { IoIosCloseCircle } from "react-icons/io";
-import { LuFeather } from "react-icons/lu";
-import {
-  MdKeyboardArrowDown,
-  MdKeyboardArrowUp,
-  MdKeyboardDoubleArrowDown,
-  MdKeyboardDoubleArrowUp,
-} from "react-icons/md";
-import { capitalize } from "@features/shared/utils/helpers/formatters/CapitalizeText";
+import ArticleHeader from "../../../table/header/ArticleHeader";
 
 export default function ArticlePreview({ studyData }: ArticlePreviewProps) {
-  const statusIconMap: Record<
-    string,
-    {
-      icon: React.ReactNode;
-      color: string;
-    }
-  > = {
-    INCLUDED: {
-      icon: <CheckCircleIcon color="green.500" />,
-      color: "green",
-    },
-    DUPLICATED: {
-      icon: <InfoIcon color="blue.500" />,
-      color: "blue",
-    },
-    EXCLUDED: {
-      icon: <IoIosCloseCircle color="red.500" size="1.4rem" />,
-      color: "red",
-    },
-    UNCLASSIFIED: {
-      icon: <WarningIcon color="yellow.500" />,
-      color: "yellow",
-    },
-  };
-
-  const priorityIconMap: Record<
-    string,
-    {
-      icon: React.ReactNode;
-      color: string;
-    }
-  > = {
-    VERY_LOW: {
-      icon: <MdKeyboardDoubleArrowDown color="#D32F2F" size="1.5rem" />,
-      color: "red",
-    },
-    LOW: {
-      icon: <MdKeyboardArrowDown color="#FBC02D" size="1.5rem" />,
-      color: "yellow",
-    },
-    HIGH: {
-      icon: <MdKeyboardArrowUp color="#F57C00" size="1.5rem" />,
-      color: "orange",
-    },
-    VERY_HIGH: {
-      icon: <MdKeyboardDoubleArrowUp color="#388E3C" size="1.5rem" />,
-      color: "green",
-    },
-  };
-
-  const selectionStatus = statusIconMap[studyData.selectionStatus];
-  const priorityLevel = priorityIconMap[studyData.readingPriority];
-
-  const pathToReference = studyData.doi;
-
-  const handleRedirectToReference = () => {
-    if (!pathToReference) return;
-    window.open(pathToReference, "_blank");
-  };
-
   return (
     <Flex
       w="100%"
@@ -92,79 +23,7 @@ export default function ArticlePreview({ studyData }: ArticlePreviewProps) {
         p=".25rem"
         h="100%"
       >
-        <Flex
-          alignItems="center"
-          justifyContent={pathToReference ? "space-between" : "end"}
-          gap="1rem"
-        >
-          {pathToReference ? (
-            <Button
-              onClick={handleRedirectToReference}
-              leftIcon={<LuFeather />}
-              fontSize="0.9rem"
-              fontWeight="bold"
-              bg="blue.600"
-              color="white"
-              borderRadius="lg"
-              boxShadow="md"
-              transition="all 0.3s ease"
-              h="2rem"
-              _hover={{
-                bg: "blue.700",
-                boxShadow: "lg",
-              }}
-              _active={{
-                bg: "blue.800",
-              }}
-            >
-              Acessar DOI
-            </Button>
-          ) : null}
-          <Flex
-            alignItems="center"
-            justifyContent="center"
-            h="2rem"
-            px={4}
-            py={2}
-            gap={2}
-            bg={`${selectionStatus.color}.100`}
-            color={`${selectionStatus.color}.700`}
-            fontWeight="semibold"
-            fontSize="1rem"
-            borderRadius="lg"
-            boxShadow="md"
-            transition="all 0.3s ease"
-            _hover={{
-              bg: `${selectionStatus.color}.200`,
-            }}
-          >
-            {selectionStatus.icon}
-            {studyData.selectionStatus}
-          </Flex>
-          <Flex
-            alignItems="center"
-            justifyContent="center"
-            h="2rem"
-            px={4}
-            py={2}
-            gap={2}
-            bg={`${priorityLevel.color}.100`}
-            color={`${priorityLevel.color}.700`}
-            fontWeight="semibold"
-            fontSize="1rem"
-            borderRadius="lg"
-            boxShadow="md"
-            transition="all 0.3s ease"
-            _hover={{
-              bg: `${priorityLevel.color}.200`,
-            }}
-          >
-            {priorityLevel.icon}
-            {capitalize(
-              studyData.readingPriority.toString().toLowerCase() || ""
-            ).replace("_", " ")}
-          </Flex>
-        </Flex>
+        <ArticleHeader studyData={studyData} />
         <Flex>
           <Text marginBottom={"7px"} w="30%" align={"left"}>
             <Text fontSize={"14px"} fontWeight={"bold"}>
