44174833e17f5dfddb02aeca5560f6455544341a
Guilherme A Mendes <guilherme.andmendes03@gmail.com>
2025-08-27 13:28:29 -0300

    feat: create multi selection response component

---
+++ src/features/review/execution-extraction/components/forms/DataExtraction/subcomponents/responses/MultiSelectionList/index.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Box, FormControl, FormLabel, Checkbox } from "@chakra-ui/react";
+
+import { capitalize } from "../../../../../../../../shared/utils/helpers/formatters/CapitalizeText";
+
+import { container, label } from "../styles";
+
+interface MultiSelectionListProps {
+  question: string;
+  options: string[];
+  answer: string[];
+  onResponse: (response: string[]) => void;
+}
+
+export default function MultiSelectionList({
+  question,
+  options,
+  answer,
+  onResponse,
+}: MultiSelectionListProps) {
+  const [selected, setSelected] = useState<string[]>(answer);
+
+  const hasIncludesValue = (value: string): string[] => {
+    if (!selected.includes(value)) return [...selected, value];
+    return selected.filter((item) => item !== value);
+  };
+
+  const handleSelectChange = (value: string) => {
+    const content = hasIncludesValue(value);
+    setSelected(content);
+    onResponse(content);
+  };
+
+  return (
+    <FormControl sx={container}>
+      <FormLabel sx={label}>{capitalize(question)}</FormLabel>
+      <Box display="flex" flexDirection="column" gap="1rem">
+        {options.map((value, index) => (
+          <Checkbox
+            checked={selected.includes(value)}
+            onChange={() => handleSelectChange(value)}
+            key={index}
+          >
+            {value}
+          </Checkbox>
+        ))}
+      </Box>
+    </FormControl>
+  );
+}
