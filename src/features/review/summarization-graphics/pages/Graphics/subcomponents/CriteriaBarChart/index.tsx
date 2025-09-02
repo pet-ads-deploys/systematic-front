83d9b33be31571e8bba8713ffa938d79ade7b5a7
montillha <marianaalmeida235b@gmail.com>
2025-09-02 17:47:19 -0300

    chore: minor updates to charts components

--- src/features/review/summarization-graphics/pages/Graphics/subcomponents/CriteriaBarChart/index.tsx
+++ src/features/review/summarization-graphics/pages/Graphics/subcomponents/CriteriaBarChart/index.tsx
@@ -3,15 +3,17 @@ import { Text } from "@chakra-ui/react";
 import BarChart from "../../../../components/charts/BarChart";
 import { useFetchStudiesByStage } from "../../../../services/useFetchStudiesByStage";
 import useFetchStudiesByCriteria from "../../../../services/useFetchStudiesByCriteria";
+import ArticleInterface from "@features/review/shared/types/ArticleInterface";
+import { StudyInterface } from "@features/review/shared/types/IStudy";
 
 type Props = {
   criteria: "inclusion" | "exclusion";
   stage: "selection" | "extraction";
+  filteredStudies: (ArticleInterface | StudyInterface)[];
 };
 
-export default function CriteriaBarChart({ criteria, stage }: Props) {
+export default function CriteriaBarChart({ criteria, stage ,filteredStudies}: Props) {
   const color = criteria === "inclusion" ? "#3c73b6" : "#C21807";
-
   const { studiesByStage, isLoadingByStage } = useFetchStudiesByStage(stage);
   const { studiesByCriteria, isLoadingByCriteria } =
     useFetchStudiesByCriteria(criteria);
@@ -19,16 +21,36 @@ export default function CriteriaBarChart({ criteria, stage }: Props) {
   if (isLoadingByCriteria || isLoadingByStage)
     return <Text>Loading chart...</Text>;
 
+
   const stageStudyIds =
     criteria === "inclusion"
       ? studiesByStage?.includedStudies.ids ?? []
       : studiesByStage?.excludedStudies.ids ?? [];
+ console.log(stageStudyIds);
+
+const filteredStudiesIds = filteredStudies
+  .map((study) => {
+    if ("studyReviewId" in study) {
+      return study.studyReviewId; //ArticleInterface
+    }
+    if ("studyId" in study) {
+      return study.studyId; //StudyInterface
+    }
+    return null;
+  })
+  .filter((id): id is number => id !== null);
+
+
+    const filteredStageIds = stageStudyIds.filter(
+    (id) => filteredStudiesIds.includes(id)
+  
+  );    
 
   const criterias = studiesByCriteria?.criteria ?? {};
   const labels = Object.keys(criterias);
   const data = labels.map(
     (label) =>
-      criterias[label].filter((id) => stageStudyIds.includes(id)).length
+      criterias[label].filter((id) =>  filteredStageIds .includes(id)).length
   );
 
   return (
@@ -37,7 +59,7 @@ export default function CriteriaBarChart({ criteria, stage }: Props) {
         criteria === "inclusion" ? "Inclusion Criteria" : "Exclusion Criteria"
       }
       labels={labels}
-      data={data /*labels.map(()=>Math.floor(Math.random()*10)+1)*/}
+      data={data}
       color={color}
     />
   );
