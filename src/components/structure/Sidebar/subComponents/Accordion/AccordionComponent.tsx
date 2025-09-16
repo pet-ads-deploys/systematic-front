5d2199323c48097df635371d324532a38c52d967
Guilherme A Mendes <guilherme.andmendes03@gmail.com>
2025-09-16 10:17:05 -0300

    refactor: remove unnecessary icons in the child sections of the accordion

--- src/components/structure/Sidebar/subComponents/Accordion/AccordionComponent.tsx
+++ src/components/structure/Sidebar/subComponents/Accordion/AccordionComponent.tsx
@@ -7,26 +7,14 @@ import {
   AccordionIcon,
   AccordionPanel,
 } from "@chakra-ui/react";
-
-import { TbAsterisk } from "react-icons/tb";
-import { MdOutlineSource } from "react-icons/md";
-import {
-  MdDescription,
-  MdHelpOutline,
-  MdCategory,
-  MdChecklist,
-  MdBarChart,
-} from "react-icons/md";
 import { MdRule } from "react-icons/md";
-import { LuFileSearch, LuFileCheck2, LuTextSelect } from "react-icons/lu";
-import { CgCheckR } from "react-icons/cg";
-import { IoBarChartSharp } from "react-icons/io5";
-import { GrTag } from "react-icons/gr";
-import { TbFilterSearch } from "react-icons/tb";
-import { VscOpenPreview } from "react-icons/vsc";
+import { LuFileSearch, LuFileCheck2 } from "react-icons/lu";
 
 // Components
 import ProtocolAccordionSubItem from "./AccordionNavItem";
+
+// Constants
+const hasShowOcultScreens = false;
 
 const AccordionComponent = () => {
   const id = localStorage.getItem("systematicReviewId");
@@ -52,49 +40,34 @@ const AccordionComponent = () => {
         </h2>
         <AccordionPanel pb={4}>
           <ProtocolAccordionSubItem
-            icon={<MdDescription size="1.2rem" color="#c9d9e5" />}
             to={`/review/planning/protocol/general-definition`}
             text="Definition"
           />
-
           <ProtocolAccordionSubItem
-            icon={<MdHelpOutline size="1.2rem" color="#c9d9e5" />}
             to={`/review/planning/protocol/research-questions/${id}`}
             text="Research"
           />
-
           <ProtocolAccordionSubItem
-            icon={<MdCategory size="1.2rem" color="#c9d9e5" />}
             to={`/review/planning/protocol/picoc/${id}`}
             text="Picoc"
           />
-
           <ProtocolAccordionSubItem
-            icon={<MdRule size="1.2rem" color="#c9d9e5" />}
             to={`/review/planning/protocol/eligibility-criteria/${id}`}
             text="Criteria"
           />
-
           <ProtocolAccordionSubItem
-            icon={<MdOutlineSource size="1.2rem" color="#c9d9e5" />}
             to={`/review/planning/protocol/information-sources-and-search-strategy/${id}`}
             text="Sources"
           />
-
           <ProtocolAccordionSubItem
-            icon={<MdChecklist size="1.2rem" color="#c9d9e5" />}
             to={`/review/planning/protocol/selection-and-extraction/${id}`}
-            text="Selection And Extraction"
+            text="Selection"
           />
-
           <ProtocolAccordionSubItem
-            icon={<TbAsterisk size="1.2rem" color="#c9d9e5" />}
             to={`/review/planning/protocol/risk-of-bias-assessment/${id}`}
             text="Risk Of Bias"
           />
-
           <ProtocolAccordionSubItem
-            icon={<MdBarChart size="1.2rem" color="#c9d9e5" />}
             to={`/review/planning/protocol/analysis-and-synthesis-of-results/${id}`}
             text="Analysys"
           />
@@ -120,23 +93,19 @@ const AccordionComponent = () => {
         </h2>
         <AccordionPanel pb={4}>
           <ProtocolAccordionSubItem
-            icon={<GrTag size="1rem" color="#c9d9e5" />}
             to={`/review/execution/identification`}
             text="Identification"
           />
           <ProtocolAccordionSubItem
-            icon={<LuTextSelect size="1.2rem" color="#c9d9e5" />}
             to={`/review/execution/selection`}
             text="Selection"
           />
           <ProtocolAccordionSubItem
-            icon={<TbFilterSearch size="1.2rem" color="#c9d9e5" />}
             to={`/review/execution/extraction`}
             text="Extraction"
           />
         </AccordionPanel>
       </AccordionItem>
-
       <AccordionItem>
         <h2>
           <AccordionButton p="1.2vw 1vh" color="white">
@@ -156,20 +125,21 @@ const AccordionComponent = () => {
         </h2>
         <AccordionPanel pb={4}>
           <ProtocolAccordionSubItem
-            icon={<IoBarChartSharp size="1rem" color="#c9d9e5" />}
             to={`/review/summarization/graphics`}
             text="Graphics"
           />
-          <ProtocolAccordionSubItem
-            icon={<VscOpenPreview size="1.2rem" color="#c9d9e5" />}
-            to={`/review/summarization/visualization`}
-            text="Visualization"
-          />
-          <ProtocolAccordionSubItem
-            icon={<CgCheckR size="1rem" color="#c9d9e5" />}
-            to={`/review/summarization/finalization`}
-            text="Finalization"
-          />
+          {hasShowOcultScreens && (
+            <>
+              <ProtocolAccordionSubItem
+                to={`/review/summarization/visualization`}
+                text="Visualization"
+              />
+              <ProtocolAccordionSubItem
+                to={`/review/summarization/finalization`}
+                text="Finalization"
+              />
+            </>
+          )}
         </AccordionPanel>
       </AccordionItem>
     </Accordion>
--- src/components/structure/Sidebar/subComponents/Accordion/AccordionNavItem.tsx
+++ src/components/structure/Sidebar/subComponents/Accordion/AccordionNavItem.tsx
@@ -1,23 +1,31 @@
-import { Box } from '@chakra-ui/react';
-import { Link } from 'react-router-dom'
-import Styles from './AccordionNavItem.module.css';
-// import { ImExit } from "react-icons/im";
-// import { FiPlusCircle } from "react-icons/fi";
-// import { ImBooks } from "react-icons/im";
+// External library
+import { Box } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
+// Styles
+import Styles from "./AccordionNavItem.module.css";
+
+// Types
 interface Props {
-    to: string;
-    text: string;
-    icon: React.ReactNode;
+  to: string;
+  text: string;
+  icon?: React.ReactNode;
 }
 
-const ProtocolAccordionSubItem = ({to, text, icon}: Props) => {
+const ProtocolAccordionSubItem = ({ to, text, icon }: Props) => {
   return (
-    <Box w='10rem' display='flex' className={Styles.accordionNavlinkBox} gap=".5rem">
+    <Box
+      w="10rem"
+      display="flex"
+      className={Styles.accordionNavlinkBox}
+      gap=".5rem"
+    >
       {icon}
-      <Link className={Styles.accordionLink} to={to}>{text}</Link>
+      <Link className={Styles.accordionLink} to={to}>
+        {text}
+      </Link>
     </Box>
-  )
-}
+  );
+};
 
 export default ProtocolAccordionSubItem;
