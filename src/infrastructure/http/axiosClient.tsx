03593e8ed89eb1b2fb352a6751ad8d62c00e7c78
Guilherme A Mendes <guilherme.andmendes03@gmail.com>
2025-09-24 19:26:27 -0300

    refactor: use env variable of define api base route

--- src/infrastructure/http/axiosClient.tsx
+++ src/infrastructure/http/axiosClient.tsx
@@ -2,7 +2,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+  baseURL: process.env.PUBLIC_API_URL,
   withCredentials: true,
 });
 
