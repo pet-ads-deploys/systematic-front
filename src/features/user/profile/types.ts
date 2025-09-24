91455fff6aab31c9bae406192b58bb3695ad0803
Guilherme A Mendes <guilherme.andmendes03@gmail.com>
2025-09-24 17:57:48 -0300

    feat: create types to profile page

--- src/features/user/profile/types.ts
+++ src/features/user/profile/types.ts
@@ -5,3 +5,9 @@ export interface Profile extends Omit<User, "password"> {
   userId: string;
   authorities: string[];
 }
+
+export type UpdateProfileDTO = Omit<
+  Profile,
+  "userId" | "username" | "authorities"
+>;
+export type Mode = "DEFAULT" | "UPDATE";
