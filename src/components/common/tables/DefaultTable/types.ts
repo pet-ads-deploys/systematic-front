89b801bdf3de1d3c2dd0602ce965093d28c2dac5
Guilherme A Mendes <guilherme.andmendes03@gmail.com>
2025-09-26 10:38:12 -0300

    feat: create title propertie to generic table component

--- src/components/common/tables/DefaultTable/types.ts
+++ src/components/common/tables/DefaultTable/types.ts
@@ -9,9 +9,11 @@ export type Column<T> = {
   key: keyof T;
   label: string;
   render?: (item: T) => React.ReactNode;
+  width?: string;
 };
 
 export interface GenericTableProps<T> {
+  title: string;
   columns: Column<T>[];
   data: T[];
 }
