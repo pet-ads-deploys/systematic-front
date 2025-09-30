70221611f5e07bb4eefccbe0eb6f0dc61895eaae
montillha <marianaalmeida235b@gmail.com>
2025-09-29 23:24:21 -0300

    layout: align numeric columns to the right in tables

--- src/features/review/summarization-graphics/components/tables/subcomponents/row/ReportTd/index.tsx
+++ src/features/review/summarization-graphics/components/tables/subcomponents/row/ReportTd/index.tsx
@@ -3,11 +3,13 @@ import { Td ,Text,Tooltip} from '@chakra-ui/react';
 type StudyCellProps = {
     text: string | string[];
     maxW?: string;
+    type?:"number" |"string"
 };
 
-export const ReportTd = ({ text, maxW = "250px" }: StudyCellProps) => {
+export const ReportTd = ({ text, maxW = "250px",type ="string"}: StudyCellProps) => {
   return (
-    <Td maxW={maxW}>
+   
+    <Td maxW={maxW} isNumeric= {type=="number"}>
       <Tooltip label={text} hasArrow>
         <Text isTruncated>{text}</Text>
       </Tooltip>
