48ae81d689431991ad7236cf6774cf437e760d70
Guilherme A Mendes <guilherme.andmendes03@gmail.com>
2025-09-30 18:35:56 -0300

    refactor: remove unnecessary context in auth module

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProvider } from "./features/shared/context/ApplicationContext";
 import { StudySelectionProvider } from "@features/review/shared/context/StudiesSelectionContext.tsx";
-import { AuthProvider } from "@features/auth/context/AuthContext.tsx";
 
 // Components
 import App from "./App.tsx";
@@ -17,13 +16,11 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <AuthProvider>
-        <AppProvider>
-          <StudySelectionProvider>
-            <App />
-          </StudySelectionProvider>
-        </AppProvider>
-      </AuthProvider>
+      <AppProvider>
+        <StudySelectionProvider>
+          <App />
+        </StudySelectionProvider>
+      </AppProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
